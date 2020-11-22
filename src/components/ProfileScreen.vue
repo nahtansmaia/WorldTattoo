b264f95a562b2d8c10bebf074367f5204997f860
Nathan Maia <nahtansmaia@gmail.com>
2020-11-22 20:22:27 -0300

    Update ProfileScreen.vue

--- src/components/ProfileScreen.vue
+++ src/components/ProfileScreen.vue
@@ -103,7 +103,7 @@ export default {
       email: "",
       phone: "",
       birthdate: "",
-      tag: "@",
+      tag: "",
       avatar:
         "https://conteudo.imguol.com.br/c/noticias/f1/2019/11/02/a-nasa-elegeu-como-foto-astronomica-do-dia-em-22-de-outubro-esta-imagem-da-via-lactea-capturada-por-jheison-huerta-no-salar-de-uyuni-na-bolivia-1572723035380_v2_900x506.jpg",
     },
